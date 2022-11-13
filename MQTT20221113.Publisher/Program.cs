1ee4dfe2c5b28ff18e1eb252784e671be366feea
Sann Lynn Htun <sannlynnhtun.error@gmail.com>
2022-11-13 11:35:08 +0630

    Add project files.

---
+++ MQTT20221113.Publisher/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MQTTnet;
+using MQTTnet.Client;
+
+Console.WriteLine("Hello, World!");
+
+var mqttFactory = new MqttFactory();
+var client = mqttFactory.CreateMqttClient();
+var options = new MqttClientOptionsBuilder()
+    //.WithClientId(Guid.NewGuid().ToString())
+    .WithTcpServer("localhost", 1883)
+    .WithCleanSession()
+    .Build();
+await client.ConnectAsync(options);
+
+Console.WriteLine("Please press a key to publish the message");
+Console.ReadLine();
+
+await PublishMessageAsync(client);
+
+await client.DisconnectAsync();
+
+async Task PublishMessageAsync(IMqttClient client)
+{
+    string messagePayloadId = "Hello";
+    var message = new MqttApplicationMessageBuilder()
+    .WithTopic("Group")
+    .WithPayload(messagePayloadId)
+    .Build();
+    if (client.IsConnected)
+    {
+        await client.PublishAsync(message);
+    }
+}
---
+++ MQTT20221113.Subscriber/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MQTTnet.Client;
+using MQTTnet;
+using MQTTnet.Server;
+using System.Text.Json;
+
+Console.WriteLine("Hello, World!");
+
+var mqttFactory = new MqttFactory();
+var client = mqttFactory.CreateMqttClient();
+var options = new MqttClientOptionsBuilder()
+    //.WithClientId(Guid.NewGuid().ToString())
+    .WithTcpServer("localhost", 1883)
+    .WithCleanSession()
+.Build();
+
+client.ApplicationMessageReceivedAsync += e =>
+{
+    Console.WriteLine("Received application message.");
+    e.DumpToConsole();
+
+    return Task.CompletedTask;
+};
+
+await client.ConnectAsync(options);
+
+Console.ReadLine();
+
+var subscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder()
+                .WithTopicFilter(
+                    f =>
+                    {
+                        f.WithTopic("Group");
+                    })
+                .Build();
+
+await client.SubscribeAsync(subscribeOptions, CancellationToken.None);
+
+await client.DisconnectAsync();
+
+internal static class ObjectExtensions
+{
+    public static TObject DumpToConsole<TObject>(this TObject @object)
+    {
+        var output = "NULL";
+        if (@object != null)
+        {
+            output = JsonSerializer.Serialize(@object, new JsonSerializerOptions
+            {
+                WriteIndented = true
+            });
+        }
+
+        Console.WriteLine($"[{@object?.GetType().Name}]:\r\n{output}");
+        return @object;
+    }
+}
